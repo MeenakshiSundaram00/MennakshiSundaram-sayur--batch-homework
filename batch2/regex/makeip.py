18bfc68b2d12ebc23f5b2c33bb91ed16e1d14e20
MeenakshiSundaram00 <sundart1201@gmail.com>
2024-04-14 13:05:36 +0530

    homework

--- batch2/regex/makeip.py
+++ batch2/regex/makeip.py
@@ -6,18 +6,60 @@ input 10026710
 output 100.2.67.10
 input 100242200
 output 100.24.220.0, 100.242.20.0'''
+import sys
 emp=[]
-def check(temp):
-    if int(temp)>255 or int(temp) <0:
-        return False
-    else:
-        return True
-def number(temp):
-    return len(temp)
-ip=int(input("enter digits"))
-while ip:
-    value=check(str(ip%1000).zfill(3))
-    if value:
-        emp.append(ip%10^(number(str(ip%1000))))
-        
-    else:
+ans=[]
+
+
+def check(a,b,c):
+    t = int(a)
+    i=t
+    j = int(b)
+    k = int(c)
+    if i == 0:
+        ans.append(i)
+        return 1
+    if i*100+j*10+k<255:
+        ans.append(i*100+j*10+k)
+        return 3
+    elif i*100+j*10:
+        ans.append(i*10+j)
+        return 2
+def printer():
+    print(*ans,sep=".")
+
+count=0
+xin=input("enter a string")
+for i in range(len(xin)):
+    emp.append(xin[i])
+while  len(emp)!=0:
+    if count==2:
+        if len(emp)==3:
+          ans.append(int(emp[0])*10+int(emp[1]))
+          ans.append(int(emp[2]))
+        if len(emp)==2:
+            ans.append(int(emp[0]))
+        printer()
+        sys.exit(0)
+    if len(emp)==2:
+      if (emp[0] == "0"):
+          ans.append(0)
+          ans.append(int(emp[1]))
+      else:
+          if int(emp[0])*10+int(emp[1])<255:
+               ans.append(int(emp[0])*10+int(emp[1]))
+          else:
+              ans.append(int(emp[0]))
+              ans.append(int(emp[1]))
+      printer()
+      sys.exit(0)
+    if len(emp) == 1:
+        ans.append(emp[0] )
+        printer()
+        sys.exit(0)
+    value=check(emp[0],emp[1],emp[2])
+    if value==3:
+        count=count+1
+    for j in range(value):
+        emp.remove(emp[0])
+print(*ans,sep=".")
