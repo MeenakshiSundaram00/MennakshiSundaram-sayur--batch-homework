7648b7159c475396a589772c687d85309cda5a31
MeenakshiSundaram00 <sundart1201@gmail.com>
2024-06-06 17:36:58 +0530

    homework

---
+++ batch2/day 12/frequency.py
@@ -0,0 +1,34 @@
+'''Problem #2
+Input is an integer list and another integer k. Output is the k most frequently occuring numbers.
+output can be in any order.
+eg input = [1,1,1,2,2,3,5,5,5,5], k =2
+output [1,5] (top 2 most frequently occuring numbers)
+input = [4,5,4,5,4,5,3,3,3,7,8,1,1,1], k = 4
+output [4,5,3,1]'''
+listofnumbefrs=[]
+copylist=[]
+answerlist=[]
+
+def frequencyreader():
+
+    counter=0
+    num=0
+    for i in copylist:
+        check=copylist.count(i)
+
+        if check>counter:
+            counter=check
+            num=i
+    answerlist.append(num)
+    for i in  range(counter):
+        copylist.remove(num)
+string=(input("enter element of string"))
+listofnumbers=string.split(',')
+for i in listofnumbers:
+    j=int(i)
+    listofnumbefrs.append(j)
+key=int(input("enter the no numbers you want to find"))
+copylist=listofnumbefrs.copy()
+for i in range(key):
+    frequencyreader()
+print(answerlist)
---
+++ batch2/day 12/sentencearrange.py
@@ -0,0 +1,49 @@
+'''Problem #1
+You have list of unique words. you input is a string with no spaces. Return true if the letters in the
+input string can be seperated into words that are in the list.
+eg list = ["we", "students", "are" ]
+input = "wearestudents"
+output = true
+eg 2 List = ["we", "doctor","and", "nurse", "are"]
+input = "wearenurseandoctor"
+output = False
+input = "wearenurseanddoctor"
+output = true'''
+import sys
+
+listofstring=[]
+def counter(string):
+    val= len(string)
+    val2=0
+    for i in listofstring:
+        for j in i:
+            val=val+1
+    if val!=val:
+        return False
+    else:
+        return True
+
+def innerelement(stringer,i):
+    if stringer==i:
+        return True
+    else:
+        return False
+
+listforstring=input("list of element")
+listofstring=listforstring.split()
+string=input("enter the string")
+val=counter(string)
+if val==False:
+    print("False")
+    sys.exit(0)
+for i in range(len(listforstring)):
+    for j in listforstring:
+        if(innerelement(string[0:len(j)],j)==True):
+            string=string[len(j):]
+    if not string:
+        break
+if not string:
+    print("true")
+else:
+    print("false")
+
